5da00aec0d164090090f55174c3ca2b3968c3974
SheerGhazi <sher.ghazi2192@gmail.com>
2024-12-07 23:26:06 +0500

    fix : Result.jsx

--- src/Components/Result/Result.jsx
+++ src/Components/Result/Result.jsx
@@ -8,6 +8,8 @@ const Result = ({input}) => {
     <div>
       Hello
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatum non. Iure, cupiditate veritatis. Harum dignissimos ratione deleniti? Sunt mollitia error quia cumque dolores vitae nostrum dolorum dolorem nobis asperiores.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium minus repellendus quasi! Laudantium quo earum architecto illo voluptatem placeat tenetur porro, vitae iusto, nemo ipsum amet laborum a ab aliquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellendus exercitationem similique saepe perspiciatis, reprehenderit quos cupiditate, consectetur harum obcaecati quibusdam quisquam asperiores labore incidunt voluptatibus accusamus explicabo nam quaerat.
     </div>
   )
 }
